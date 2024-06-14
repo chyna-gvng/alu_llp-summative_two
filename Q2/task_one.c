9e7fd3cd737386074c155f57c84f3e4c5f940eef
Chyna <w.angoye@alustudent.com>
2024-06-14 17:19:19 +0300

    map graph by line numbers

--- Q2/task_one.c
+++ Q2/task_one.c
@@ -1,85 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>          // Line 1
+#include <stdlib.h>         // Line 2
+#include <string.h>         // Line 3
 
-typedef struct {
-    char fullName[100];
-    float grade;
-} Student;
+typedef struct {            // Line 4
+    char fullName[100];     // Line 5
+    float grade;            // Line 6
+} Student;                  // Line 7
 
-void merge(Student arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+void merge(Student arr[], int left, int mid, int right) {       // Line 8
+    int n1 = mid - left + 1;                                    // Line 9
+    int n2 = right - mid;                                       // Line 10
     
-    Student *L = (Student *)malloc(n1 * sizeof(Student));
-    Student *R = (Student *)malloc(n2 * sizeof(Student));
+    Student *L = (Student *)malloc(n1 * sizeof(Student));       // Line 11
+    Student *R = (Student *)malloc(n2 * sizeof(Student));       // Line 12
 
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+    for (int i = 0; i < n1; i++)                                // Line 13
+        L[i] = arr[left + i];                                   // Line 14
+    for (int j = 0; j < n2; j++)                                // Line 15
+        R[j] = arr[mid + 1 + j];                                // Line 16
 
-    int i = 0, j = 0, k = left;
+    int i = 0, j = 0, k = left;                                 // Line 17
 
-    while (i < n1 && j < n2) {
-        if (L[i].grade >= R[j].grade) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
+    while (i < n1 && j < n2) {                                  // Line 18
+        if (L[i].grade >= R[j].grade) {                         // Line 19
+            arr[k] = L[i];                                      // Line 20
+            i++;                                                // Line 21
+        } else {                                                // Line 22
+            arr[k] = R[j];                                      // Line 23
+            j++;                                                // Line 24
         }
-        k++;
+        k++;                                                    // Line 25
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
+    while (i < n1) {            // Line 26
+        arr[k] = L[i];          // Line 27
+        i++;                    // Line 28
+        k++;                    // Line 29
     }
 
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while (j < n2) {            // Line 30
+        arr[k] = R[j];          // Line 31
+        j++;                    // Line 32
+        k++;                    // Line 33
     }
 
-    free(L);
-    free(R);
-}
+    free(L);                    // Line 34
+    free(R);                    // Line 35
+}                               // Line 36
 
-void mergeSort(Student arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
+void mergeSort(Student arr[], int left, int right) {            // Line 37
+    if (left < right) {                                         // Line 38
+        int mid = left + (right - left) / 2;                    // Line 39
         
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
+        mergeSort(arr, left, mid);                              // Line 40
+        mergeSort(arr, mid + 1, right);                         // Line 41
         
-        merge(arr, left, mid, right);
+        merge(arr, left, mid, right);                           // Line 42
     }
-}
+}                                                               // Line 43
 
-int main() {
-    int n;
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
+int main() {                                                    // Line 44
+    int n;                                                      // Line 45
+    printf("Enter the number of students: ");                   // Line 46
+    scanf("%d", &n);                                            // Line 47
     
-    Student *students = (Student *)malloc(n * sizeof(Student));
+    Student *students = (Student *)malloc(n * sizeof(Student)); // Line 48
     
-    for (int i = 0; i < n; i++) {
-        printf("Enter full name for student %d: ", i + 1);
-        scanf(" %[^\n]s", students[i].fullName);
-        printf("Enter grade for student %d: ", i + 1);
-        scanf("%f", &students[i].grade);
+    for (int i = 0; i < n; i++) {                               // Line 49
+        printf("Enter full name for student %d: ", i + 1);      // Line 50
+        scanf(" %[^\n]s", students[i].fullName);                // Line 51
+        printf("Enter grade for student %d: ", i + 1);          // Line 52
+        scanf("%f", &students[i].grade);                        // Line 53
     }
     
-    mergeSort(students, 0, n - 1);
+    mergeSort(students, 0, n - 1);                                                         // Line 54
     
-    printf("Sorted list of students:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Full Name: %s, Grade: %.2f\n", students[i].fullName, students[i].grade);
+    printf("Sorted list of students:\n");                                                  // Line 55
+    for (int i = 0; i < n; i++) {                                                          // Line 56
+        printf("Full Name: %s, Grade: %.2f\n", students[i].fullName, students[i].grade);   // Line 57
     }
 
-    free(students);
-    return 0;
-}
+    free(students);         // Line 58
+    return 0;               // Line 59
+}                           // Line 60
